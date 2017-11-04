05b09d18e4fa04c446e5381b1b904afaa406c193
Will Timpson <will.timpson@gmail.com>
2017-11-04 18:25:28 -0400

    Revert "move to material next"
    This reverts commit c6eca29d6f9201ed9bc440935ccc65781332b744.

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Grid from "material-ui/Grid";
 
 import AuthorizedUser from "./components/AuthorizedUser";
@@ -14,15 +15,17 @@ import TextField from "material-ui/TextField";
 class App extends Component {
   render() {
     return (
-      <Grid container alignItems="flex-start" direction="row" justify="center">
-        <Router>
-          <Switch>
-            <Route exact path="/:id" component={PrimaryUser} />
-            <Route exact path="/authorized/:id" component={AuthorizedUser} />
-            <Route exact path="/:primaryId/:id" component={AuthorizedUser} />
-          </Switch>
-        </Router>
-      </Grid>
+      <MuiThemeProvider>
+        <Grid alignItems="flex-start" direction="row" justify="center">
+          <Router>
+            <Switch>
+              <Route exact path="/:id" component={PrimaryUser} />
+              <Route exact path="/authorized/:id" component={AuthorizedUser} />
+              <Route exact path="/:primaryId/:id" component={AuthorizedUser} />
+            </Switch>
+          </Router>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,32 @@
 import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 
-const theme = createMuiTheme({
-  card: {
-    titleColor: "#FFFFFF"
-  },
-  textField: {
-    textColor: "#606A77"
-  },
-  palette: {
-    primary1Color: "#004975",
-    primary2Color: "#D52833",
-    primary3Color: "#0775AB",
-    accent1Color: "#004975",
-    accent2Color: "#D52833",
-    accent3Color: "#0775AB",
-    alternateTextColor: "#0775AB",
-    disabledColor: "#E4E4E3",
-    canvasColor: "#004975",
-    pickerHeaderColor: "#004975",
-    textColor: "#FFFFFF",
-    borderColor: "#004975"
-  }
-});
+const theme = {
+  muiTheme: getMuiTheme({
+    card: {
+      titleColor: "#FFFFFF"
+    },
+    textField: {
+      textColor: "#606A77"
+    },
+    palette: {
+      primary1Color: "#004975",
+      primary2Color: "#D52833",
+      primary3Color: "#0775AB",
+      accent1Color: "#004975",
+      accent2Color: "#D52833",
+      accent3Color: "#0775AB",
+      alternateTextColor: "#0775AB",
+      disabledColor: "#E4E4E3",
+      canvasColor: "#004975",
+      pickerHeaderColor: "#004975",
+      textColor: "#FFFFFF",
+      borderColor: "#004975"
+    }
+  })
+};
 
 export default ({ children }) => (
-  <MuiThemeProvider theme={theme} children={children} />
+  <MuiThemeProvider {...theme} children={children} />
 );
