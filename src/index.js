63d42cf0f6ee99d625d0ee46455c099729b54f16
idhalverson <halversoncode@gmail.com>
2017-11-06 13:36:59 -0500

    removed outer router

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./app/App";
 import registerServiceWorker from "./registerServiceWorker";
 import Theme from "./theme";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
-    <Route path="/">
-      <Theme>
-        <App />
-      </Theme>
-    </Route>
-  </Router>,
+  <Theme>
+    <App />
+  </Theme>,
   document.getElementById("root")
 );
 
